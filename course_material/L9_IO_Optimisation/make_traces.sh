4744e089bab3e78c3a76bba6938eaa99391d47c9
Toby Potter <tobympotter@gmail.com>
2022-08-22 18:20:09 +0800

    Finished L9 IO Optimisation

--- course_material/L9_IO_Optimisation/make_traces.sh
+++ course_material/L9_IO_Optimisation/make_traces.sh
@@ -12,13 +12,13 @@ rm -rf $taudir
 mkdir -p $taudir
 
 # Get a trace for the calculation with synchronous IO
-tau_exec -T serial -opencl ./wave2d_sync.exe
+tau_exec -T serial -opencl ./wave2d_sync.exe -gpu
 cd $taudir; echo 'y' | tau_treemerge.pl
 tau_trace2json ./tau.trc ./tau.edf -chrome -ignoreatomic -o trace_sync.json
 
 # Get a trace for the calculation with asynchronous IO
 cd $experiment_dir
-tau_exec -T serial -opencl ./wave2d_async.exe
+tau_exec -T serial -opencl ./wave2d_async.exe -gpu
 cd $taudir; echo 'y' | tau_treemerge.pl
 tau_trace2json ./tau.trc ./tau.edf -chrome -ignoreatomic -o trace_async.json
 
