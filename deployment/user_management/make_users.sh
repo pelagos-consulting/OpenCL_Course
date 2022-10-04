98684f1ab4e0e0034194b689204fdf8c56c69b6f
Toby Potter <tobympotter@gmail.com>
2022-10-04 16:37:56 +0800

    latest

---
+++ deployment/user_management/make_users.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+fname=users.csv
+rm $fname
+
+for n in $(seq -f "%02g" 1 99)
+do
+    pass=$(gpw 1)
+    usr="lab$n"
+
+    # Check if user exists
+    if id -u "$usr" > /dev/null 1>&1
+    then
+        # User exists
+    else
+        # Create the user and add to groups
+        adduser $usr 
+        adduser $usr video
+        adduser $usr render
+    fi
+
+    #change password for user
+    echo "$usr:$pass" >> $fname
+    
+done
