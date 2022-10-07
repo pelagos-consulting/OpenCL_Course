abac42babbd18e1cfe1062a0da2fc749293bde4e
Toby Potter <tobympotter@gmail.com>
2022-10-07 12:04:01 +0800

    Latest

--- deployment/user_management/make_users.sh
+++ deployment/user_management/make_users.sh
@@ -3,7 +3,9 @@
 fname=users.csv
 fname_export=users_export.csv
 rm $fname
+rm $fname_export
 
+let count=1
 for n in $(seq -f "%02g" 1 99)
 do
     pass=$(gpw 1)
@@ -13,21 +15,24 @@ do
     if id -u "$usr" > /dev/null 1>&1
     then
         # User exists
-        echo "$usr exists"
-    else
-        # Create the user and add to groups
-        adduser $usr --disabled-password --gecos "" 
-    fi
+        userdel -rf $usr
+	groupdel $usr
+    fi 
+    
+    # Add the user back
+    groupadd $usr --gid $((2000+count))
+    adduser $usr --disabled-password --gecos "" --uid $((2000+count)) --gid $((2000+count))
 
     # Check if jupyter-user exists
     if id -u "jupyter-$usr" > /dev/null 1>&1
     then
-        # User exists
-        echo "jupyter-$usr exists"
-    else
-        # Create the user and add to groups
-        adduser jupyter-$usr --disabled-password --gecos "" 
+        userdel -rf jupyter-$usr
+	groupdel jupyter-$usr
     fi
+
+    # Create the user and add to groups
+    groupadd jupyter-$usr --gid $((3000+count))
+    adduser jupyter-$usr --disabled-password --gecos "" --uid $((3000+count)) --gid $((3000+count))
 
     adduser $usr video
     adduser $usr render
@@ -39,7 +44,7 @@ do
     echo "$usr:$pass" >> $fname
     echo "$usr:$pass" >> $fname_export   
     echo "jupyter-$usr:$pass" >> $fname
-    
+    let count++
 done
 
 # Batch change user passwords
