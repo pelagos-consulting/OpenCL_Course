e7b9d8a5422ded8372ddca13165144abc290749b
Toby Potter <tobympotter@gmail.com>
2023-03-01 15:45:12 +1100

    finished doing local memory example

--- course_material/L6_Memory_Management/mat_mult_local.cpp
+++ course_material/L6_Memory_Management/mat_mult_local.cpp
@@ -231,7 +231,7 @@ int main(int argc, char** argv) {
     size_t work_dim=2;
     
     // Desired local size
-    const size_t local_size[]={ 16, 4 };
+    const size_t local_size[]={ 8, 8 };
     
     // Desired global_size
     const size_t global_size[]={ N1_C, N0_C };
